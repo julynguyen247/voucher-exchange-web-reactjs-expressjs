179b28e8cbe58ceb02bbcea2a462f389f9718710
julynguyen247 <julyneverdie@gmail.com>
2025-03-10 11:27:36 +0700

    create voucher

--- src/server.js
+++ src/server.js
@@ -2,15 +2,19 @@ const express = require("express");
 const apiRoutes = require("./routes/api");
 const connection = require("./config/database");
 require("dotenv").config();
-const cors = require('cors')
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
 const app = express();
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
 //config cors
 app.use(cors());
+//config file upload
+app.use(fileUpload());
 //config req.body
 app.use(express.json()); // for json
 app.use(express.urlencoded({ extended: true })); // for form data
+
 app.use("/v1/api", apiRoutes);
 app.get("/", (req, res) => {
   res.send("Hello World!");
