3e283a760b6e19b12630b8ce0b2fbf54351be910
Ducky <huuduc6805@gmail.com>
2025-03-30 22:16:38 +0700

    update server for security

--- be/src/server.js
+++ be/src/server.js
@@ -2,12 +2,16 @@ const express = require("express");
 const apiRoutes = require("./routes/api");
 const connection = require("./config/database");
 require("dotenv").config();
-const cors = require('cors');
+const cors = require("cors");
 const path = require("path");
-const fileUpload = require('express-fileupload');
+const fileUpload = require("express-fileupload");
 const app = express();
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
+const compression = require("compression");
+const sharp = require("sharp");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
 
 //config cors
 app.use(cors());
@@ -17,8 +21,28 @@ app.use(fileUpload());
 app.use(express.json()); // for json
 app.use(express.urlencoded({ extended: true })); // for form data
 
+// Add security headers
+//app.use(helmet());
 
-app.use("/images", express.static(path.join(__dirname, "public/images")));
+// Enable gzip compression
+app.use(compression());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Maximum requests per windowMs
+  message: "Too many requests from this IP, please try again later.",
+});
+
+app.use("/v1/api", limiter);
+
+app.use(
+  "/images",
+  express.static(path.join(__dirname, "public/images"), {
+    maxAge: "1y", // Cache for 1 year
+    etag: false,
+  })
+);
+
 app.use("/v1/api", apiRoutes);
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -29,7 +53,7 @@ app.get("/", (req, res) => {
     //using mongoose
     await connection();
 
-    app.listen(port,hostname, () => {
+    app.listen(port, hostname, () => {
       console.log(`Backend Nodejs App listening on port ${port}`);
     });
   } catch (error) {
